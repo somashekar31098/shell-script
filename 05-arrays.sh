96eaa9df9e6d035a1d70c5f0a864bef1e56f6bb2
somashekar31098 <somashekar31098@gmail.com>
2024-04-26 17:01:20 +0530

    shell script practise

---
+++ 05-arrays.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+movies=("rrr" "pokiri" "billa")
+
+#size of above array is 3
+#index 0,1,2 
+
+echo "first movie is ={movie[0]}"
+echo "first movie is ={movie[1]}"
+echo "first movie is ={movie[2]}"
