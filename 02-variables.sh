4fed0b6a165fadcbf692ed8bc2b901091894614b
somashekar31098 <somashekar31098@gmail.com>
2024-04-26 16:17:53 +0530

    shell script practise

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=suresh
+person2=ramesh
+
+echo "person1:: hi ramesh how are you "
+echo "person2:: yeah, suresh i am good , how are you "
+echo "suresh:: i am good , what about your job "
+echo "ramesh:: i am learning devops , changing career "
