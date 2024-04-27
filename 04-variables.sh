c992b09cfaf66f02f0dff3a03fcfdaa392aa5057
somashekar31098 <somashekar31098@gmail.com>
2024-04-27 10:39:40 +0530

    shell script

--- 04-variables.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-person1=$1
-person2=$2
-
-echo "$person1:: hi $person2 how are you "
-echo "$person2:: yeah, $person1 i am good , how are you "
-echo "$person1:: i am good , what about your job "
-echo "$person2:: i am learning devops , changing career "
