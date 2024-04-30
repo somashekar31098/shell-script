9e3ba4c7e1b6ef0a94f4d8b12d5b9d0101780e77
somashekar31098 <somashekar31098@gmail.com>
2024-04-30 14:41:40 +0530

    conditions

---
+++ 12-colours.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R=$(\e[31m)
+Y=$(\e[32m)
+N=$(\e[Om)
+
+validate(){
+   if [ $1 -ne 0 ]
+    then 
+       echo " $2 is .... $R FAILURE $N "
+       exit 1
+    else
+       echo " $2 is .... $R sucess $N"
+   fi 
+}
+
+if [ $USERID -ne 0 ]
+then 
+   echo "run the script with root access "
+   exit 1
+else
+   echo "you are super user "
+fi
+
+dnf install mysql -y &>>$LOGFILE
+validate $? "installing mysql"
+
+dnf install git -y &>>$LOGFILE
+validate $? "installation of git"
