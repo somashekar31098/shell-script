7b53e6396d1be0942e9378f28d4ee351152c4625
somashekar31098 <somashekar31098@gmail.com>
2024-04-30 09:56:09 +0530

    conditions;git push origin main
    git add .:git commit -mconditions:git push origin main
    git add .;git commit -m conditions

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -2,9 +2,9 @@
 
 number=$1
 
-if [ $number -gt 10 ]
-then
-echo "given number $number is greater than 10"
-else
-echo "given number $number is lesser than 10"
+if [ $number -gt 10]
+ then
+   echo "iven number $number is grater than 10"
+ else
+   echo "given number $number is lesser than 10"
 fi
