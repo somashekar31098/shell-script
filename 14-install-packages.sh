26684324f02683802fcc2bdfeeb2afa8d13aa453
somashekar31098 <somashekar31098@gmail.com>
2024-04-30 15:56:41 +0530

    conditions

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -5,6 +5,16 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPTNAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
 
+validate(){
+   if [ $1 -ne 0 ]
+    then 
+       echo " $i is installed skipping "
+       exit 1
+    else
+       echo " $i is not installed , need to install"
+   fi 
+}
+
 if [ $USERID -NE O ]
   then 
   echo "run the script with root access"
@@ -17,7 +27,8 @@ for i in $@
 do 
 
 echo "package to install: $i"
-
+  dnf list installed $i
+  validate $1 "installing packages"
 done 
 
 
