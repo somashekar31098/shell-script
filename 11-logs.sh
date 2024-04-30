f30fa0e8c1901c80af9bfce9014ad6680dd11b68
somashekar31098 <somashekar31098@gmail.com>
2024-04-30 13:18:01 +0530

    conditions

---
+++ 11-logs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$($0 | cut -d "." -f)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+
+validate(){
+   if [ $1 -ne 0 ]
+    then 
+       echo " $2 is ....FAILURE"
+       exit 1
+    else
+       echo " $2 is ....sucess"
+   fi 
+}
+
+if [ $USERID -ne 0 ]
+then 
+   echo "run the script with root access "
+   exit 1
+else
+   echo "you are super user "
+fi
+
+dnf install mysql -y &>>$LOGFILE
+validate $? "installing mysql"
+
+dnf install git -y &>>$LOGFILE
+validate $? "installation of git"
