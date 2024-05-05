439a0ea77e12b0b7d227f444eee385bf35b6c574
somashekar31098 <somashekar31098@gmail.com>
2024-05-05 11:35:00 +0530

    calling script

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -5,7 +5,8 @@ COURSE="Devops from current script"
 echo "before calling other script, course: $COURSE"
 echo "process id of current script : $$"
 
-./other-script.sh
+source ./other-script.sh
 
 echo "after calling the other script, course: $COURSE"
 
+ 
