af7d0310df81423bdad11da1ca92ac35c6f98db0
somashekar31098 <somashekar31098@gmail.com>
2024-05-05 12:39:25 +0530

    calling script

--- 15-first-script.sh
+++ 15-first-script.sh
@@ -5,6 +5,6 @@ COURSE="devops from first script"
 echo "before calling second script, course: $COURSE"
 echo "processing id of first script: $$"
 
-./16-second-script.sh
+source ./16-second-script.sh
 
 echo "after calling second script, course: $COURSE"
